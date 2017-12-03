cb8d2ddbe92b34ca7c929f0580adeb83c4730ef4
wyb314 <13212777425@163.com>
2017-12-03 20:51:49 +0800

    移植中

--- CyclonePhysicsUnity/Assets/Scripts/Engine/ParticleContactResolver.cs
+++ CyclonePhysicsUnity/Assets/Scripts/Engine/ParticleContactResolver.cs
@@ -23,7 +23,7 @@ namespace CyclonePhysics
         public void resolveContacts(List<ParticleContact> contactArray,FP duration )
         {
             this.iterationsUsed = 0;
-            int contactArrayCount = contactArray.Count;
+            int numContacts = contactArray.Count;
 
             while (this.iterationsUsed < this.iterations)
             {
@@ -31,19 +31,52 @@ namespace CyclonePhysics
 
                 int maxIdx = int.MaxValue;
 
-                for (int i = 0; i < contactArrayCount; i++)
+                for (int i = 0; i < numContacts; i++)
                 {
                     FP sepVel = contactArray[i].calculateSeparatingVelocity();
 
-                    if (sepVel < max && (sepVel < 0 || contactArray[i].penetratoin > 0))
+                    if (sepVel < max && (sepVel < 0 || contactArray[i].penetration > 0))
                     {
                         max = sepVel;
                         maxIdx = i;
                     }
-
                 }
 
+                if (maxIdx == numContacts)
+                {
+                    break;
+                }
+
+                contactArray[maxIdx].resolve(duration);
+                TSVector[] move = contactArray[maxIdx].particleMovements;
+
+                for (int i = 0; i < numContacts; i++)
+                {
+                    if (contactArray[i].particles[0] == contactArray[maxIdx].particles[0])
+                    {
+                        contactArray[i].penetration -= move[0]*contactArray[i].contactNormal;
+                    }
+                    else if (contactArray[i].particles[0] == contactArray[maxIdx].particles[1])
+                    {
+                        contactArray[i].penetration -= move[1]*contactArray[i].contactNormal;
+                    }
+
+                    if (contactArray[i].particles[1] != null)
+                    {
+                        if (contactArray[i].particles[1] == contactArray[maxIdx].particles[0])
+                        {
+                            contactArray[i].penetration += move[0]*contactArray[i].contactNormal;
+                        }
+                        else if(contactArray[i].particles[1] == contactArray[maxIdx].particles[1])
+                        {
+                            contactArray[i].penetration += move[1]*contactArray[i].contactNormal;
+                        }
+                    }
+
+                }
             }
+
+            
 
         }
 
